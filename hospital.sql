be573f1539b4afa686556ad577f5090f69d3bb8f
AndriyShymoniak <andryshimonyak@gmail.com>
2019-11-06 10:05:24 +0200

    initial commit

---
+++ hospital.sql
@@ -0,0 +1,2 @@
+DROP DATABASE IF EXISTS hospital_project;
+CREATE DATABASE hospital_project;
---
+++ src/test/java/com/shymoniak/hospital/HospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shymoniak.hospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
